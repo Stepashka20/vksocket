887aeaacf53e85166b0010be438a615d3791eea8
Stepan <stepa.davydov.2002@mail.ru>
2020-10-03 17:20:40 +0300

    Updates

---
+++ precache-manifest.f2dfc7bbce11f4e1962a94c6e85caa51.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7952a4c685afa717cbbbdd129f6fe3",
+    "url": "/vksocket/index.html"
+  },
+  {
+    "revision": "a40bd948613ef2056821",
+    "url": "/vksocket/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a95ea6835b81de962b22",
+    "url": "/vksocket/static/css/main.4ef4d824.chunk.css"
+  },
+  {
+    "revision": "a40bd948613ef2056821",
+    "url": "/vksocket/static/js/2.0bd9eb29.chunk.js"
+  },
+  {
+    "revision": "8c323eee9c0820e0e208cd7dbdc413ce",
+    "url": "/vksocket/static/js/2.0bd9eb29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a95ea6835b81de962b22",
+    "url": "/vksocket/static/js/main.7183112a.chunk.js"
+  },
+  {
+    "revision": "d3eed99905ac1692fd23",
+    "url": "/vksocket/static/js/runtime-main.58984e6d.js"
+  },
+  {
+    "revision": "d9408e7ce732a2964c10e1ceb5c33db1",
+    "url": "/vksocket/static/media/coinpic.d9408e7c.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vksocket/static/media/persik.4e1ec840.png"
+  }
+]);
